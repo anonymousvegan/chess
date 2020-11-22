efb278a31becf895cc320ce9f5e3afbafc14b331
nikola <majjunjulavgust@gmail.com>
2020-11-22 17:06:33 +0100

    sve firure su selektovane

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="sr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Šah</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <div id="kontejner">
+        <!--prvi red -->
+        <div id="1" class="belo">
+            <div id="topcrnilevi">
+                <img src="figure/crni-top.png" alt="">
+            </div>
+        </div>
+        <div id="2" class="crno">
+            <div id="konjcrnilevi">
+                <img src="figure/crni-konj.png" alt="">
+            </div>
+        </div>
+        <div id="3" class="belo">
+            <div id="lovaccrnilevi">
+                <img src="figure/crni-lovac.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="kraljicacrna">
+                <img src="figure/crna-kraljica.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="kraljcrni">
+                <img src="figure/crni-kralj.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+              <div id="lovaccrnidesni">
+                <img src="figure/crni-lovac.png" alt="">
+              </div>
+        </div>
+        <div class="belo">
+            <div id="konjcrnidesni">
+                <img src="figure/crni-konj.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="topcrnidesni">
+                <img src="figure/crni-top.png" alt="">
+            </div>
+        </div>
+        <!--drugi red -->
+        <div  class="crno">
+            <div id="pesakcrni1">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakcrni2">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakcrni3">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakcrni4">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakcrni5">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakcrni6">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakcrni7">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakcrni8">
+                <img src="figure/crni-pesak.png" alt="">
+            </div>
+        </div>
+        <!--treći red -->
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <!--četvrti red-->
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <!--peti red -->
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <!--šesti red -->
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <div class="crno"></div>
+        <div class="belo"></div>
+        <!--sedmi red -->
+        <div class="belo">
+            <div id="pesakbeli1">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakbeli2">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakbeli3">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakbeli4">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakbeli5">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakbeli6">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="pesakbeli7">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="pesakbeli8">
+                <img src="figure/beli-pesak.png" alt="">
+            </div>
+        </div>
+        <!-- osmi red -->
+        <div class="crno">
+            <div id="topbelilevi">
+                <img src="figure/beli-top.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="konjbelilevi">
+                <img src="figure/beli-konj.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="lovacbelilevi">
+                <img src="figure/beli-lovac.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="kraljicabela">
+                <img src="figure/bela-kraljica.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="kraljbeli">
+                <img src="figure/beli-kralj.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="lovacbelidesni">
+                <img src="figure/beli-lovac.png" alt="">
+            </div>
+        </div>
+        <div class="crno">
+            <div id="konjbelidesni">
+                <img src="figure/beli-konj.png" alt="">
+            </div>
+        </div>
+        <div class="belo">
+            <div id="topbelidesni">
+                <img src="figure/beli-top.png" alt="">
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,57 @@
+var figure=[]
+class figura{
+    constructor(boja, ime, x, y, e){
+        this.boja= boja;
+        this.ime=ime;
+        this.x=x;
+        this.y=y;
+        this.e=e;
+    }
+}
+//crne-figure
+CrniTopLevo= new figura("crna", "top", 0, 0, document.querySelector("#topcrnilevi"));
+CrniKonjlevo= new figura("crna", "konj", 1, 0,  document.querySelector("#konjcrnilevi"));
+CrniLovacLevo= new figura("crna", "lovac", 2, 0,  document.querySelector("#lovaccrnilevi"));
+CrnaKraljica= new figura("crna", "kraljica", 3, 0,  document.querySelector("#kraljicacrna"));
+CrniKralj= new figura("crna", "kralj", 4, 0,  document.querySelector("#kraljcrni"));
+CrniLovacDesno= new figura("crna", "lovac", 5, 0, document.querySelector("#lovaccrnidesni"));
+CrniKonjDesno= new figura("crna", "Konj", 6, 0,  document.querySelector("#konjcrnidesni"));
+CrniTopDesno= new figura("crna", "top", 7, 0,  document.querySelector("#topcrnidesni"));
+crniPesak0= new figura("crna", "pesak", 0, 1,  document.querySelector("#pesakcrni1"));
+crniPesak1= new figura("crna", "pesak", 1, 1,  document.querySelector("#pesakcrni2"));
+crniPesak2= new figura("crna", "pesak", 2, 1,  document.querySelector("#pesakcrni3"));
+crniPesak3= new figura("crna", "pesak", 3, 1,  document.querySelector("#pesakcrni4"));
+crniPesak4= new figura("crna", "pesak", 4, 1,  document.querySelector("#pesakcrni5"));
+crniPesak5= new figura("crna", "pesak", 5, 1,  document.querySelector("#pesakcrni6"));
+crniPesak6= new figura("crna", "pesak", 6, 1,  document.querySelector("#pesakcrni7"));
+crniPesak7= new figura("crna", "pesak", 7, 1,  document.querySelector("#pesakcrni8"));
+//bele figure
+beliPesak0= new figura("bela", "pesak", 0, 6, document.querySelector("#pesakbeli1"));
+beliPesak1= new figura("bela", "pesak", 1, 6, document.querySelector("#pesakbeli2"));
+beliPesak2= new figura("bela", "pesak", 2, 6, document.querySelector("#pesakbeli3"));
+beliPesak3= new figura("bela", "pesak", 3, 6, document.querySelector("#pesakbeli4"));
+beliPesak4= new figura("bela", "pesak", 4, 6, document.querySelector("#pesakbeli5") );
+beliPesak5= new figura("bela", "pesak", 5, 6, document.querySelector("#pesakbeli6"));
+beliPesak6= new figura("bela", "pesak", 6, 6, document.querySelector("#pesakbeli7"));
+beliPesak7= new figura("bela", "pesak", 7, 6, document.querySelector("#pesakbeli8"));
+beliTopLevo= new figura("bela", "top", 0, 7, document.querySelector("#topbelilevi"));
+beliKonjlevo= new figura("bela", "konj", 1, 7, document.querySelector("#konjbelilevi"));
+beliLovacLevo= new figura("bela", "lovac", 2, 7, document.querySelector("#lovacbelilevi"));
+belaKraljica= new figura("bela", "kraljica", 3, 7,  document.querySelector("#kraljicabela"));
+beliKralj= new figura("bela", "kralj", 4, 7, document.querySelector("#kraljbeli"));
+beliLovacDesno= new figura("bela", "lovac", 5, 7, document.querySelector("#lovacbelidesni"));
+beliKonjDesno= new figura("bela", "Konj", 6, 7, document.querySelector("#konjbelidesni"));
+beliTopDesno= new figura("bela", "top", 7, 7, document.querySelector("#topbelidesni"));
+figure=[CrniTopLevo,CrniKonjlevo, CrniLovacLevo, CrnaKraljica, CrniKralj,
+    CrniLovacDesno, CrniKonjDesno, CrniTopDesno, crniPesak0, crniPesak1, 
+    crniPesak2, crniPesak3, crniPesak4, crniPesak5, crniPesak6, crniPesak7,
+    beliPesak0, beliPesak1, beliPesak2, beliPesak3, beliPesak4, beliPesak5,
+    beliPesak6, beliPesak7, beliTopLevo, beliKonjlevo, beliLovacLevo, belaKraljica, 
+    beliKralj, beliLovacDesno, beliKonjDesno, beliTopDesno];
+for (i=0; i<figure.length; i++){
+    console.log(i)
+    figure[i].e.addEventListener("click", select);
+}
+function select(){
+    this.classlist.togle("")
+}
